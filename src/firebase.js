863060192b6ab09c37428f16219640ab6266292f
scottie-schneider <scott@laclave.co>
2020-01-18 15:56:30 -0500

    adding semantic ui

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBlSUbiXXwHMXvcgfecbmlwXql8-PzaO_U",
+  authDomain: "react-slack-clone-44e75.firebaseapp.com",
+  databaseURL: "https://react-slack-clone-44e75.firebaseio.com",
+  projectId: "react-slack-clone-44e75",
+  storageBucket: "react-slack-clone-44e75.appspot.com",
+  messagingSenderId: "113338155854",
+  appId: "1:113338155854:web:40c4433b35caeab3cc941f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+
 import registerServiceWorker from './registerServiceWorker';
+
+import 'semantic-ui-css/semantic.min.css'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const Root = () => (
   <Router>
     <Switch>
-      <Route path="/" component={App}/>
+      <Route exact path="/" component={App}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/register" component={Register}/>
     </Switch>
   </Router>
 )
