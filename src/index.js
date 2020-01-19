ac2627d36640f428fe7efe0261fd0cc3d12e1e83
scottie-schneider <scott@laclave.co>
2020-01-19 11:26:18 -0500

    scaffolding app components, and adding sign out functionality in firebase + reducers for signout + redirect if no user to signin page

--- src/index.js
+++ src/index.js
@@ -10,17 +10,30 @@ import firebase from './firebase';
 import 'semantic-ui-css/semantic.min.css'
 
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom'
+import Spinner from './Spinner';
+
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser, clearUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
-  componentDidMount() {
+  componentDidMount() {        
     firebase.auth().onAuthStateChanged(user => {
       if(user){
+        this.props.setUser(user)
         this.props.history.push('/');
+      } else {
+        this.props.history.push('/login');
+        this.props.clearUser();
       }
     })
   }
   render() {
-    return (
+    return this.props.isLoading ? <Spinner /> : (
       <Switch>
         <Route exact path="/" component={App}/>
         <Route path="/login" component={Login}/>
@@ -29,6 +42,16 @@ class Root extends React.Component {
     )
   }
 }
-const RootWithAuth = withRouter(Root)
-ReactDOM.render(<Router><RootWithAuth /></Router>, document.getElementById('root'));
+
+const mapStateToProps = state => ({
+  isLoading: state.user.isLoading
+})
+const RootWithAuth = withRouter(connect(mapStateToProps, { setUser, clearUser })(Root))
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>, 
+  document.getElementById('root'));
 registerServiceWorker();
